7143a7174beef61ae99a0ce70da7d6fc39a36177
Svantje Lilienthal <svantje.lilienthal@wikimedia.de>
2021-09-10 12:58:35 +0200

    display images of secrets (#2)

---
+++ src/components/SPARQLQueryDispatcher.js
@@ -0,0 +1,12 @@
+export class SPARQLQueryDispatcher {
+    constructor( endpoint ) {
+        this.endpoint = endpoint;
+    }
+
+    query( sparqlQuery ) {
+        const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+        const headers = { 'Accept': 'application/sparql-results+json' };
+
+        return fetch( fullUrl, { headers } ).then( body => body.json() );
+    }
+}
